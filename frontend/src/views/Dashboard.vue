b2ee2cc12e48674a79ba135de1bc50cb28c724ce
Lars Schellhas <lars.schellhas@outlook.com>
2021-10-24 23:17:46 +0200

    Center text on Dashboard and Home

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -1,12 +1,25 @@
 <template>
-  <div class="dashboard">
-    <h2>Erfolgreich eingeloggt</h2>
+  <div
+    class="dashboard d-flex justify-content-center m-4"
+  >
+    <h2>{{ title }}</h2>
   </div>
 </template>
 
 <script>
+import { computed } from 'vue'
+import { useStore } from 'vuex'
 
 export default {
-  name: 'Dashboard'
+  name: 'Dashboard',
+  setup () {
+    const store = useStore()
+
+    const title = computed(() => {
+      return `Hello ${store.getters['user/getFirstName']} ${store.getters['user/getLastName']}! Great day for fishin', ain't it?`
+    })
+
+    return { title }
+  }
 }
 </script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home">
+  <div class="home dashboard d-flex justify-content-center m-4">
     <h2>SaaS-Boilerplate for Vue.JS and Django</h2>
   </div>
 </template>
