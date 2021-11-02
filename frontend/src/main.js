d1c890e067692a343d97c1d34f1a2e3f27bd5330
Lars Schellhas <lars.schellhas@outlook.com>
2021-11-02 23:27:37 +0100

    Install and use vue-i18n for localization

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,5 +4,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import i18n from './localization'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(i18n).mount('#app')
