92109d6bde2d6689056923b51dc48b2347ce45c8
Lars Schellhas <lars.schellhas@outlook.com>
2021-11-03 11:43:53 +0100

    Remove duplicated "use" statement

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,4 +6,4 @@ import router from './router'
 import store from './store'
 import i18n from './i18n'
 
-createApp(App).use(i18n).use(store).use(router).use(i18n).mount('#app')
+createApp(App).use(i18n).use(store).use(router).mount('#app')
