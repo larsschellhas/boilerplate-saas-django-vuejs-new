d14007465193360647807def6dcf608dfcbe2673
Lars Schellhas <lars.schellhas@outlook.com>
2021-11-03 11:42:45 +0100

    Reinstall i18n with VueCLI

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import i18n from './localization'
+import i18n from './i18n'
 
-createApp(App).use(store).use(router).use(i18n).mount('#app')
+createApp(App).use(i18n).use(store).use(router).use(i18n).mount('#app')
