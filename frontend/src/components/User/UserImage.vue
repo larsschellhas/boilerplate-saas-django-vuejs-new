149c71a2a3bc5fe74372bdeb1a041202bcb3c841
Lars Schellhas <lars.schellhas@outlook.com>
2021-11-10 15:34:42 +0100

    Add user-image class to UserImage component

--- frontend/src/components/User/UserImage.vue
+++ frontend/src/components/User/UserImage.vue
@@ -1,14 +1,14 @@
 <template>
   <img
     v-if="imgUrl"
-    class="rounded-circle"
+    class="rounded-circle user-image"
     :src="imgUrl"
     :height="size"
     :width="size"
   >
   <i
     v-else
-    class="fas fa-user-circle"
+    class="fas fa-user-circle user-image"
     :style="`font-size: ${size}px; width: ${size}px;`"
   />
 </template>
