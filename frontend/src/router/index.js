6777be8ca1c15163c4a5f7379adf12e71525bebd
Lars Schellhas <lars.schellhas@outlook.com>
2021-11-13 12:43:13 +0100

    Add "scroll to hash/element" behaviour to router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -56,7 +56,23 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
+  scrollBehavior (to, from, savedPosition) {
+    if (to.hash) {
+      return {
+        el: to.hash,
+        left: 0,
+        top: 64,
+        behavior: 'smooth'
+      }
+    } else {
+      return {
+        left: 0,
+        top: 0,
+        behavior: 'smooth'
+      }
+    }
+  }
 })
 
 router.beforeEach((to, from, next) => {
