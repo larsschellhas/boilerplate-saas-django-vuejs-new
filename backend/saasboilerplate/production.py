d44a8370ded1277aaafb49bdbdef5edda0da9933
Lars Schellhas <lars.schellhas@outlook.com>
2021-11-23 17:03:33 +0100

    Use safer and faster production settings

--- backend/saasboilerplate/production.py
+++ backend/saasboilerplate/production.py
@@ -3,15 +3,25 @@ import environ
 
 env = environ.Env()
 
-# Sets django and dj-stripe to run in live mode
+### Activate live mode & disable debug
 DEBUG = False
 STRIPE_LIVE_MODE = True
 
-# WhiteNoise configuration
+
+### Security related settings for production
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
+### WhiteNoise configuration for serving static files
 MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+
+### Database settings
+# Set database connection lifetime to one day to reduce reconnects
+CONN_MAX_AGE = 86400
 # DBHOST is only the server name, not the full URL
 dbhostname = env("DBHOST")
 
@@ -26,5 +36,3 @@ DATABASES = {
         "PASSWORD": env("DBPASS"),
     }
 }
-
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
