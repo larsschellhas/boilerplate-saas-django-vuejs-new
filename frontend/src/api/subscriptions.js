39b905846de805205fde3826ad0bb80a46cc74e3
Lars Schellhas <lars.schellhas@outlook.com>
2021-11-29 23:07:43 +0100

    Parse product metadata JSON fields if available

--- frontend/src/api/subscriptions.js
+++ frontend/src/api/subscriptions.js
@@ -1,12 +1,20 @@
 import { api } from '@/api/axiosConfig'
 
+function parsePotentialJSON (data) {
+  try {
+    return JSON.parse(data)
+  } catch {
+    return data
+  }
+}
+
 export default {
   async getProducts () {
     try {
       const response = await api.get('products/')
       for (const product in response.data.results) {
         for (const metadataEntry in response.data.results[product].metadata) {
-          response.data.results[product].metadata[metadataEntry] = JSON.parse(response.data.results[product].metadata[metadataEntry])
+          response.data.results[product].metadata[metadataEntry] = parsePotentialJSON(response.data.results[product].metadata[metadataEntry])
         }
       }
       return { success: true, data: response.data }
