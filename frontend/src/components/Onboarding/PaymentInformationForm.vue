abd160b395230e996057d95ce561748aae7d24c1
Lars Schellhas <lars.schellhas@outlook.com>
2022-03-14 23:15:06 +0100

    Add components for sign-up page

---
+++ frontend/src/components/Onboarding/PaymentInformationForm.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>PaymentInformationForm</h1>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ frontend/src/components/Onboarding/PlanSelection.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="container text-center p-4">
+    <div class="form-switch d-flex justify-content-center align-items-center mb-4">
+      <label
+        class="form-check-label"
+        :class="{'fw-bold': !monthlyInterval}"
+        for="switchInterval"
+      >
+        {{ t('components.productOverview.yearlyPayment') }}
+      </label>
+      <input
+        id="switchInterval"
+        v-model="monthlyInterval"
+        class="form-check-input mx-2"
+        type="checkbox"
+        role="switch"
+      >
+      <label
+        class="form-check-label"
+        :class="{'fw-bold': monthlyInterval}"
+        for="switchInterval"
+      >
+        {{ t('components.productOverview.monthlyPayment') }}
+      </label>
+    </div>
+    <div class="row row-cols-1 row-cols-md-3">
+      <div
+        v-for="product in products"
+        :key="product"
+        class="col"
+      >
+        <ProductCard
+          :product="product"
+          :interval="interval"
+          @product-selection="(data) => {handleSelection(productId = data.productId, priceId = data.priceId)}"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed, onMounted, ref } from 'vue'
+import ProductCard from '@/components/Product/ProductCard'
+import { useI18n } from 'vue-i18n'
+import { useStore } from 'vuex'
+
+// Enable access to localizations
+const { t } = useI18n()
+// Enable access to vuex store
+const store = useStore()
+
+const products = computed(() => {
+  return store.getters['subscriptions/getProducts']
+})
+
+const monthlyInterval = ref(true)
+const interval = computed(() => {
+  return monthlyInterval.value ? 'month' : 'year'
+})
+
+function handleSelection (productId, priceId) {
+  console.log(productId)
+  console.log(priceId)
+}
+
+onMounted(() => {
+  store.dispatch('subscriptions/retrieveProducts')
+})
+</script>
