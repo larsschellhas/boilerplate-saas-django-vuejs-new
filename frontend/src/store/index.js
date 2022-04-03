615da8cf46075fef2b9ab54dcc4c20d11c4f12e6
Lars Schellhas <lars.schellhas@outlook.com>
2022-04-03 13:58:06 +0200

    Remove subscriptions from store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,7 +1,6 @@
 import { createStore } from 'vuex'
 import createPersistedState from 'vuex-persistedstate'
 import localization from './modules/localization'
-import subscriptions from './modules/subscriptions'
 import user from './modules/user'
 import { storeLocalizationPlugin } from '@/i18n'
 
@@ -15,7 +14,6 @@ export default createStore({
   },
   modules: {
     localization,
-    subscriptions,
     user
   },
   plugins: [
